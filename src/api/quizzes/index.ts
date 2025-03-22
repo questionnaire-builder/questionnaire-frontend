163d38cf4875156fc220135ce8297b27229e1f86
yuriivintoniak <you@example.com>
2025-03-22 19:58:40 +0200

    feat: add api function for fetching all quizzes

---
+++ src/api/quizzes/index.ts
@@ -0,0 +1,10 @@
+import { httpClient } from "..";
+
+const PATH_PREFIX = "/api/quizzes";
+
+export const GET_ALL_QUIZZES = "GET_ALL_QUIZZES";
+
+export const getAllQuizzes = async () => {
+  const { data } = await httpClient.get(`${PATH_PREFIX}`);
+  return data;
+};
