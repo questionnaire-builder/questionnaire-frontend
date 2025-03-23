2ab233cfd718ea47c864da213981d7b5a9765894
yuriivintoniak <you@example.com>
2025-03-23 22:35:46 +0200

    feat: add api function for creating quiz

--- src/api/quizzes/index.ts
+++ src/api/quizzes/index.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { httpClient } from "..";
 
 const PATH_PREFIX = "/api/quizzes/";
@@ -14,4 +15,16 @@ export const DELETE_QUIZ_BY_ID = "DELETE_QUIZ_BY_ID";
 export const deleteQuizById = async (quizId: string) => {
   const { data } = await httpClient.delete(`${PATH_PREFIX}/${quizId}`);
   return data;
+};
+
+export const CREATE_QUIZE = "CREATE_QUIZE";
+
+export const createQuiz = async (payload: { name: string; description: string }) => {
+  try {
+    const { data } = await httpClient.post(`${PATH_PREFIX}`, payload);
+    return data;
+  } catch (error) {
+    if (!axios.isAxiosError(error)) throw error;
+    throw error.response?.data;
+  }
 };
